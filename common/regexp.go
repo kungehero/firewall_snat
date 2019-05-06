fe4e1d53a4ab181d4c1232d23af1a703de57be66
“kun.zhang10 <kun.zhang10@ucarinc.com>
2019-05-06 15:36:20 +0800

    update reg

--- common/regexp.go
+++ common/regexp.go
@@ -25,24 +25,21 @@ func (snat *SnatValues) GetIPFromSnat(shellReturnValue string) {
 func (snat *SnatValues) GetIPValueFromTCP(ips []string, shellReturnValue string) {
 	son := sync.Map{}
 	snat.IPFirewallValue = sync.Map{}
-	count := 0
 	for _, v := range ips {
-		if count > 0 {
-			reg := regexp.MustCompile(fmt.Sprintf(`IP:%v+[^)]*`, v))
-			arr := reg.FindString(shellReturnValue)
-			reg1 := regexp.MustCompile(`\d+`)
-			arr1 := reg1.FindAllString(arr, -1)
-			if len(arr1) > 0 {
-				son.Store("usedrate", arr1[6])
-				son.Store("used", arr1[7])
-				son.Store("total", arr1[8])
-				son.Store("available", arr1[9])
-				snat.IPFirewallValue.Store(v, son)
-			} else {
-				fmt.Println("GetIPValueFromTCP data is null", len(arr1))
-				return
-			}
+		reg := regexp.MustCompile(fmt.Sprintf(`IP:%v+[^)]*`, v))
+		arr := reg.FindString(shellReturnValue)
+		reg1 := regexp.MustCompile(`\d+`)
+		arr1 := reg1.FindAllString(arr, -1)
+		if len(arr1) < 9 {
+			fmt.Println("GetIPValueFromTCP data is null", len(arr1))
+			continue
+		} else {
+			son.Store("usedrate", arr1[6])
+			son.Store("used", arr1[7])
+			son.Store("total", arr1[8])
+			son.Store("available", arr1[9])
+			snat.IPFirewallValue.Store(v, son)
+
 		}
-		count++
 	}
 }
